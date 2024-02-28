18b6b45d0fdd3a48b29b1d735cc2e666893d6847
Nazeerahamed <nazeernh@gmail.com>
2024-02-28 09:35:29 +0530

    Add files via upload

---
+++ arrage.py
@@ -0,0 +1,20 @@
+input = [0,1,0,1,0,0,1,1,1,0]
+
+#new_list = [x for x in input if x == 0] + [x for x in input if x == 1]
+#print(new_list)
+
+#################
+##   OR        ##
+#################
+
+new_list = []
+for i in input:
+  if i == 0:
+    new_list.insert(0,i)
+  else:
+    new_list.append(i)
+
+print(new_list)
+
+
+
